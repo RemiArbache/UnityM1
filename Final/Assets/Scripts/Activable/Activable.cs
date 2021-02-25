8b91f2fe7978bf1943eb85bd68cca1fa911ae6e2
RemiArbache <32673658+RemiArbache@users.noreply.github.com>
2021-02-25 15:09:25 +0100

    Remove broken project

--- Final/Assets/Scripts/Activable/Activable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class Activable : MonoBehaviour
-{
-        public bool activated = false;
-}
--- Final/Assets/Scripts/VoiceClipsScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "VoiceClips", menuName = "clips", order = 0)]
-public class VoiceClipsScriptableObject : ScriptableObject
-{
-    [SerializeField] public AudioClip[] voiceLines = new AudioClip[]{};
-
-}
